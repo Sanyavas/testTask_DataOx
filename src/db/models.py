3b162a315979ab8f66cf8efe25e3536314f30c8b
Sanyavas <vasilinaoleksandr@gmail.com>
2025-01-11 15:28:34 +0200

    add models

---
+++ src/db/models.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Seller(Base):
+    __tablename__ = 'sellers'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    phone_number = Column(String, nullable=True)
+    rating = Column(Float, nullable=True)
+    registered_date = Column(DateTime, nullable=True)
+    last_active_date = Column(DateTime, nullable=True)
+    location = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
+
+    products = relationship("Product", back_populates="seller")
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=True)
+    condition = Column(String, nullable=True)
+    is_olx_delivery = Column(Boolean, nullable=True)
+    size = Column(String, nullable=True)
+    is_private = Column(Boolean, nullable=True)
+    site_id = Column(String, unique=True, nullable=False)
+    views_count = Column(Integer, nullable=True)
+    description = Column(String, nullable=True)
+    image_url = Column(String, nullable=True)
+    product_url = Column(String, nullable=False)
+    published_date = Column(DateTime, nullable=True)
+    seller_id = Column(Integer, ForeignKey('sellers.id'), nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
+
+    seller = relationship("Seller", back_populates="products")
